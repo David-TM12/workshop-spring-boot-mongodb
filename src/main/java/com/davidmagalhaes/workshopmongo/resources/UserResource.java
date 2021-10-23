aeea24cf7219222a7cde35b19ef8d290a303f5e8
DavidTM <deivi_gf144@hotmail.com>
2021-10-23 11:19:48 -0400

    Endpoint para retornar os posts de um usuario

--- src/main/java/com/davidmagalhaes/workshopmongo/resources/UserResource.java
+++ src/main/java/com/davidmagalhaes/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.davidmagalhaes.workshopmongo.resources;
 
+import com.davidmagalhaes.workshopmongo.domain.Post;
 import com.davidmagalhaes.workshopmongo.domain.User;
 import com.davidmagalhaes.workshopmongo.dto.UserDTO;
 import com.davidmagalhaes.workshopmongo.services.UserService;
@@ -54,5 +55,12 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+
+    @GetMapping(value = "/{id}/posts")
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 }
